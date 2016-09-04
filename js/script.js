3a8dd62b9d542a6fa1d69137edd7269191f43c2c
Arthuriot <alinaamberlanda@Alinas-MacBook-Pro.local>
2016-09-04 16:02:09 +0100

    JavaScript и Pixel Perfect

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
 
 var link = document.querySelector(".search-btn");
 var popup = document.querySelector(".booking-form");
+var datein = popup.querySelector("[name = datein]");
+var dateout = popup.querySelector("[name = dateout]");
 
 link.addEventListener("click", function(event) {
-if (popup.classList.contains("booking-form-show")) {
-event.preventDefault();
-popup.classList.remove("booking-form-show");
-}
-else {
-event.preventDefault();
-popup.classList.add("booking-form-show");
-}
+  if (popup.classList.contains("booking-form-show")) {
+    event.preventDefault();
+    popup.classList.remove("booking-form-show");
+    popup.classList.remove("booking-form-error");
+  }
+  else {
+    event.preventDefault();
+    popup.classList.add("booking-form-show");
+  }
+  datein.focus();
+});
 
+popup.addEventListener("submit", function(event) {
+  if (!datein.value || !dateout.value || !adult.value || !kids.value) {
+    event.preventDefault();
+    popup.classList.remove("booking-form-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("booking-form-error");
+  }
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("booking-form-show")) {
+      popup.classList.remove("booking-form-show");
+      popup.classList.remove("booking-form-error");
+    }
+  }
 });
